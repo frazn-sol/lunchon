0a05b760e0ac85d7dc124da3a22aa0d3230f3841
David Blackmon <davidkblackmon@gmail.com>
2012-10-02 20:21:45 -0400

    image uploading

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,6 +1,8 @@
 class Deal < ActiveRecord::Base
   has_many :comments
-  attr_accessible :name, :price
+  attr_accessible :name, :price, :image
   validates_numericality_of :price
   validates_presence_of :name, :price
+
+  mount_uploader :image, ImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120623202043) do
+ActiveRecord::Schema.define(:version => 20121002233132) do
 
   create_table "comments", :force => true do |t|
     t.integer  "deal_id"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(:version => 20120623202043) do
     t.decimal  "price"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "image"
   end
 
 end
