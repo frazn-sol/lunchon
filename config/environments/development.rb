7de62cdddb6e1d67edf00afad04ce05cbda15ea3
David Blackmon <davidkblackmon@gmail.com>
2012-10-28 23:54:15 -0400

    add correct default url

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,5 +35,5 @@ Lunchon::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  config.action_mailer.default_url_options = { :host => 'fanhub.dev' }
+  config.action_mailer.default_url_options = { :host => 'lunchon.dev' }
 end
