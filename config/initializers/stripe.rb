07c9b2ea614f4376383625096be14608a67939e2
David Blackmon <davidkblackmon@gmail.com>
2014-06-29 14:23:52 -0400

    set stripe to use the production account

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,2 +1,7 @@
+# test keys
+# Stripe.api_key = 'sk_test_L1FgJz0cQR3lSw1CndnXNhWU'
+# STRIPE_PUBLIC_KEY = 'pk_test_jEoyX390SclhC6eObnoRsPyd'
+
+# live keys
 Stripe.api_key = 'sk_test_L1FgJz0cQR3lSw1CndnXNhWU'
-STRIPE_PUBLIC_KEY = 'pk_test_jEoyX390SclhC6eObnoRsPyd'
+STRIPE_PUBLIC_KEY = 'pk_live_gohqRTEzq5vHezHRNKpLwtbP'
