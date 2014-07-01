e79375c04ed0014776cb0175e9d0e2038ccc683b
David Blackmon <davidkblackmon@gmail.com>
2014-06-30 21:18:23 -0400

    use the proper api_key for stripe

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -3,5 +3,5 @@
 # STRIPE_PUBLIC_KEY = 'pk_test_jEoyX390SclhC6eObnoRsPyd'
 
 # live keys
-Stripe.api_key = 'sk_test_L1FgJz0cQR3lSw1CndnXNhWU'
+Stripe.api_key = 'sk_live_kQC8STDehymytHujmMvTiBtI'
 STRIPE_PUBLIC_KEY = 'pk_live_gohqRTEzq5vHezHRNKpLwtbP'
