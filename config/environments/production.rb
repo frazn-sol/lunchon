9b72e984f39d1ddc67488b8c5a5c53cbd1599881
frazn-sol <fraz.ahsan17@gmail.com>
2014-08-08 20:23:31 +0500

    Task3 and 5 completed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,6 @@ Lunchon::Application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
-  config.assets.initialize_on_precompile = false
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
@@ -20,7 +19,7 @@ Lunchon::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
